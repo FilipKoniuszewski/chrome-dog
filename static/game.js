7515e3f264ad8878a6c4fc46853da60fb6924fe9
DominikBerniak <berniak.dominik@gmail.com>
2021-08-30 15:37:55 +0200

    Updated objects

--- static/game.js
+++ static/game.js
@@ -1,45 +1,41 @@
 window.addEventListener("load", function (){
     const game = {
         init: function (){
-            const gameChar = document.getElementById("game-character");
-            let posX = 0;
-            let posY = 450;
-            let enemy = document.getElementById("game-enemy");
-            let posEnemy = 980;
-
-            this.startGame(enemy, posEnemy)
-            this.initKeyEvents(gameChar, posX, posY)
+            this.startGame(this.enemy.enemy, this.enemy.posEnemy, this.gameChar.gameChar, this.gameChar.posY)
+            this.initKeyEvents(this.gameChar.gameChar, this.gameChar.posY)
         },
-        startGame : function (enemy, posEnemy){
+        gameChar : {
+            gameChar: document.getElementById("game-character"),
+            posY: 450
+        },
+        enemy : {
+            enemy: document.getElementById("game-enemy"),
+            posEnemy: 980
+        },
+        startGame : function (){
             setInterval(function (){
-                posEnemy -= 10;
-                enemy.style.left = posEnemy + "px";
-                if (posEnemy === 0){
-                    enemy.remove();
+                game.enemy.posEnemy -= 1;
+                game.enemy.enemy.style.left = game.enemy.posEnemy + "px";
+                if (game.enemy.posEnemy === 0){
+                    game.enemy.enemy.remove();
                 }
-            },1000)
+                if (game.gameChar.posY < 450){
+                    game.gameChar.posY += 5;
+                    game.gameChar.gameChar.style.top = game.gameChar.posY + "px";
+                }
+            },10)
         },
-        initKeyEvents : function (gameChar, posX, posY){
+        initKeyEvents : function (){
             window.addEventListener("keydown", function (event) {
-                if (event.key === "ArrowRight"){
-                    if (posX < 950) {
-                        posX += 25;
-                        gameChar.style.left = posX + "px";
-                    }
-                }else if (event.key === "ArrowLeft"){
-                    if (posX > 0) {
-                        posX -= 25;
-                        gameChar.style.left = posX + "px";
-                    }
-                }else if (event.key === "ArrowUp") {
-                    if (posY > 0) {
-                        posY -= 25;
-                        gameChar.style.top = posY + "px";
+                if (event.key === " " || event.key === "ArrowUp") {
+                    if (game.gameChar.posY > 0) {
+                        game.gameChar.posY -= 50;
+                        game.gameChar.gameChar.style.top = game.gameChar.posY + "px";
                     }
                 }else if (event.key === "ArrowDown"){
-                    if (posY < 450) {
-                        posY += 25;
-                        gameChar.style.top = posY + "px";
+                    if (game.gameChar.posY < 450) {
+                        game.gameChar.posY += 25;
+                        game.gameChar.gameChar.style.top = game.gameChar.posY + "px";
                     }
                 }
             })
