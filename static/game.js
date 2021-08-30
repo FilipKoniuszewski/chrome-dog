0e694568ba6173289d9d0c4b5d35bfbbde922157
DominikBerniak <berniak.dominik@gmail.com>
2021-08-30 14:51:47 +0200

    Basic version

--- static/game.js
+++ static/game.js
@@ -1,6 +1,30 @@
-const game = {
-    init: function (){
+window.addEventListener("load", function (){
+        const game = {
+            init: function () {
+                const gameChar = document.getElementById("game-character");
+                let posX = 0;
+                let posY = 450;
+                let posEnemy = 980;
+                let enemy = document.getElementById("game-enemy")
+            },
+            addKeysEvents: function (){
 
-        
-    }
-};
+
+        }
+    };
+    setInterval(function (){
+        posEnemy -= 10;
+        enemy.style.left = posEnemy + "px";
+        if (posX == posEnemy){
+            alert("UPS");
+        }
+        if (posEnemy == 0){
+            enemy.remove();
+        }
+    },10)
+
+
+
+    })
+
+})
